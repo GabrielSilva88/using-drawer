4d6014fc7f8292d0bc897fb2135aa637fc797ff0
GabrielSilva88 <gabrielmg.silva@outlook.com>
2024-01-12 17:29:25 -0300

    Seção 7: Navegações- 57. Evolução nas navegações, correção do erro ao rodar aplicativo.

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+// npm install @react-navigation/native;
+import { NavigationContainer } from '@react-navigation/native';
+
+// npm install @react-navigation/native-stack;
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Home from './src/pages/Home';
+import Sobre from './src/pages/Sobre';
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name='Home'
+          component={Home}
+          options={{
+            title: 'Tela inicio',
+            headerStyle: {
+              backgroundColor: '#121212'
+            },
+            headerTintColor: '#fff',
+            headerShown: true
+          }}
+        />
+        <Stack.Screen
+          name='Sobre'
+          component={Sobre}
+          options={{
+            title: 'Pagina Sobre'
+          }}
+
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/pages/Home/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import { useNavigation } from '@react-navigation/native';
+
+export default function Home() {
+    const navigation = useNavigation();
+    return (
+    <View style={styles.container}>
+      <Text>Tela Home</Text>
+      <Button 
+        title='Ir para sobre'
+        onPress={()=> navigation.navigate('Sobre')}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
